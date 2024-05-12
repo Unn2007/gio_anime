491481f043587efbf8bec173120b1d7e8ac447c1
Unn2007 <unn2007@i.ua>
2024-05-12 18:38:21 +0300

    lesson4

--- js/mainData.js
+++ js/mainData.js
@@ -1,3 +1,8 @@
-const mainData = () {
-    
+const mainData = () => {
+    fetch("https://test-edc1f-default-rtdb.firebaseio.com/anime.json").then((response)=>{
+        return response.json()
+
+    }).then((data)=>{console.log(data)})
 }
+
+mainData()
--- js/modal.js
+++ js/modal.js
@@ -5,11 +5,11 @@ const modalClose=modal.querySelector(".search-close-switch")
 const searchInput = modal.querySelector("#search-input")
 
 modalBtn.addEventListener("click",()=>{
-    modal.style.display="block";
+    modal.classList.add("active");
 })
 
 modalClose.addEventListener("click",()=>{
-    modal.style.display="none"
+    modal.classList.remove("active")
 })
 
 searchInput.addEventListener("input", (event) => {
